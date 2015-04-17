4ac5a07203644be2d4428c613662e83a8b8d1376
ersin <ersin_the_ertan@hotmail.com>
2015-04-17 16:54:42 -0400

    Fixed Quantifiers and groups, was missing the matcher.find(); to initiate the groupings, only one find is needed to look at groups no matter what group(n);
    Did some group changes and edge cases.
    Starting lookarounds.

--- app/src/androidTest/java/com/nullcognition/javaregularexpressions/chapter03/AdvancedRegex.java
+++ app/src/androidTest/java/com/nullcognition/javaregularexpressions/chapter03/AdvancedRegex.java
@@ -14,10 +14,10 @@ import java.util.regex.Pattern;
 	positive lookaheads, negative lookaheads, positive lookbehinds, and negative lookbehinds.
 */
 
-   // read blog for qualifiers example, look arounds should be easy enough to understand
 
 public class AdvancedRegex extends AndroidTestCase {
 
+   // REMEMBER THAT THE matcher.find() IS NEEDED TO WORK WITH GROUPS, code is fixed look at examples
 
    public void testGroupsNonCapt(){
 
@@ -26,50 +26,78 @@ public class AdvancedRegex extends AndroidTestCase {
 	  assertEquals(true, Pattern.matches("(.)((?:b|d).)", "abc")); // dealing with noncapturing groups, there is no group(3)
 
 	  Pattern p = Pattern.compile("a((b)c)");
-	  Pattern pp = Pattern.compile("a((?:b)c)"); // should not be captured
+	  Pattern pp = Pattern.compile("a(?:(b)c)"); // should not be captured
 
 	  Matcher m = p.matcher("abc"); // which should match for both
 	  Matcher mm = pp.matcher("abc");
 
+	  m.find();
+	  mm.find();
 	  // Log.e(getClass().getSimpleName(), m.group(2)); // either you can't log with debugging, or groups dont work
 
 	  // Log.e(getClass().getSimpleName(), mm.group(2));
 
-	  assertNotSame(mm.group(2), m.group(2)); // group to should not exist
+	  assertEquals("bc", m.group(1)); // group one is bc
+	  assertEquals("b", m.group(2)); // group c is b in m
+
+	  assertEquals("b", mm.group(1)); // but group one is b is mm
    }
 
    public void testGroups(){
 
 	  Pattern pat = Pattern.compile("(A1)");
 	  Matcher mat = pat.matcher("A1");
-	  String str = mat.group(0); // outputs a1 // don't know the problem
+	  mat.find();
+	  String str = mat.group(0); // outputs a1 // don't know the problem // to fix mat.find() to return the first group
 
 	  assertEquals("A1", str);
    }
+
+   // greedy will match more than it is needed but will release if next quantifier needs the char for a match
+   public void testGreedyQuantifiers(){
+
+	  String[] inputs = {"aabcdeeee", "aabce", "aabe", "aabccccccccde"};
+
+	  // non capture, match exactly 2 times, zero or more, zero or one, one or more
+	  Pattern pattern = Pattern.compile("(?:a{2})(bc*)(d?)(e+)"); // note that the astrisk quantifier is only for the c in group 2, move it
+	  // to the right of the ( parentheses to apply it to the whole group, see inputs[2]
+
+	  Matcher matcher;
+
+	  for(String _inputs : inputs){
+		 matcher = pattern.matcher(_inputs);
+		 assertTrue(matcher.matches());
+	  }
+   }
+
+   public void testPossessiveQuantifiers(){
+
+	  assertFalse(Pattern.matches("\\w++\\w", "TwoWords")); // the possessive \w++ does not give up the second word for \w to match
+	  // remember, in java the extra \ is needed on the \ for the regex engine to recognize it as a regex command
+   }
+
+   public void testReluctantQuantifier(){
+
+	  Pattern pattern = Pattern.compile("(\\d+?)"); // group(0) is the whole match only
+	  Matcher matcher = pattern.matcher("123");
+
+	  boolean isMatch = matcher.find(); // required to move to the next occurance of the group, to work with groups
+
+	  assertEquals("1", matcher.group(0)); // group 0, and 1(only if there is a () group around the pattern) show the same thing,
+	  // should only be the matched group of 1 due to the reluctant qualifier that will be
+	  // satisfied with the first character it gets
+
+	  Pattern pattern1 = Pattern.compile("\\d+?\\d+?");
+	  Matcher matcher1 = pattern1.matcher("123");
+
+	  // matcher1.find(); double matches do not work in this specific case
+	  matcher1.find(); // with out this, should fail
+	  assertEquals("12", matcher1.group(0));
+
+   }
+
+   public void testLookarounds(){
+
+   }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- app/src/androidTest/java/com/nullcognition/javaregularexpressions/chapter04/ObjectOrientedRegex.java
+++ app/src/androidTest/java/com/nullcognition/javaregularexpressions/chapter04/ObjectOrientedRegex.java
@@ -2,9 +2,16 @@ package com.nullcognition.javaregularexpressions.chapter04;
 
 import junit.framework.TestCase;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by ersin on 16/04/15 at 6:10 PM
  */
 public class ObjectOrientedRegex extends TestCase {
+
+
+
+
 
 }
